134c43a2535c7a8b272dbe1171124e2b79f15404
Björn Linse <bjorn.linse@gmail.com>
2022-07-10 23:24:27 +0200

    atachima

--- src/eiri.zig
+++ src/eiri.zig
@@ -1,7 +1,30 @@
 const std = @import("std");
-const BPF = std.os.linux.BPF;
+const linux = std.os.linux;
+const BPF = linux.BPF;
 const Insn = BPF.Insn;
 
+const fd_t = linux.fd_t;
+const errno = linux.getErrno;
+
+pub fn prog_test_run(
+    prog: fd_t,
+) !u32 {
+    var attr = BPF.Attr{
+        .test_run = std.mem.zeroes(BPF.TestRunAttr),
+    };
+
+    attr.test_run.prog_fd = prog;
+
+    const rc = linux.bpf(.prog_load, &attr, @sizeOf(BPF.TestRunAttr));
+    return switch (errno(rc)) {
+        .SUCCESS => attr.test_run.retval,
+        .ACCES => error.UnsafeProgram,
+        .FAULT => error.BPFProgramFault,
+        .INVAL => error.InvalidProgram,
+        .PERM => error.AccessDenied,
+        else => |err| std.os.unexpectedErrno(err),
+    };
+}
 pub fn main() !void {
     const good_prog = [_]Insn{
         Insn.mov(.r0, 0),
@@ -9,5 +32,6 @@ pub fn main() !void {
     };
 
     const prog = try BPF.prog_load(.socket_filter, &good_prog, null, "MIT", 0);
+    _ = try prog_test_run(prog);
     defer std.os.close(prog);
 }
