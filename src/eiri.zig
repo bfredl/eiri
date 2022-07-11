a23d172e0d6d8b7fe3095865a3fdf7bc5792c818
Björn Linse <bjorn.linse@gmail.com>
2022-07-11 09:31:49 +0200

    HAiiIII

--- src/eiri.zig
+++ src/eiri.zig
@@ -6,6 +6,8 @@ const Insn = BPF.Insn;
 const fd_t = linux.fd_t;
 const errno = linux.getErrno;
 
+const p = std.debug.print;
+
 pub fn prog_test_run(
     prog: fd_t,
 ) !u32 {
@@ -14,24 +16,31 @@ pub fn prog_test_run(
     };
 
     attr.test_run.prog_fd = prog;
+    attr.test_run.repeat = 1;
 
-    const rc = linux.bpf(.prog_load, &attr, @sizeOf(BPF.TestRunAttr));
+    var dummy_data = [1]u8{0} ** 32;
+    attr.test_run.data_size_in = dummy_data.len;
+    attr.test_run.data_in = @ptrToInt(&dummy_data);
+
+    const rc = linux.bpf(.prog_test_run, &attr, @sizeOf(BPF.TestRunAttr));
+    // TODO: check the docs for actually expected errors
     return switch (errno(rc)) {
         .SUCCESS => attr.test_run.retval,
         .ACCES => error.UnsafeProgram,
         .FAULT => error.BPFProgramFault,
-        .INVAL => error.InvalidProgram,
+        .INVAL => error.InvalidArgument,
         .PERM => error.AccessDenied,
         else => |err| std.os.unexpectedErrno(err),
     };
 }
 pub fn main() !void {
     const good_prog = [_]Insn{
-        Insn.mov(.r0, 0),
+        Insn.mov(.r0, 3),
         Insn.exit(),
     };
 
     const prog = try BPF.prog_load(.socket_filter, &good_prog, null, "MIT", 0);
-    _ = try prog_test_run(prog);
+    const retval = try prog_test_run(prog);
+    p("RETURNERA: {}\n", .{retval});
     defer std.os.close(prog);
 }
