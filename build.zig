3ea74170f59f77954cd04cb499e5b329cec126f7
bfredl <bjorn.linse@gmail.com>
2022-11-01 14:37:18 +0100

    remove unused build code

--- build.zig
+++ build.zig
@@ -2,13 +2,7 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     var exe = b.addExecutable("eiri", "src/eiri.zig");
-    // exe.addPackagePath("bpf", "libs/bpf/exports.zig");
     exe.setBuildMode(mode);
     // exe.use_stage1 = true;
     exe.install();
-
-    const connect = b.step("connect", "connecto wired");
-    const run = exe.run();
-    run.step.dependOn(b.getInstallStep());
-    connect.dependOn(&run.step);
 }
